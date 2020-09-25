a924f0a29ab1ddcba1e6f1a02d11e007b439cf3a
Виктория <55043240+zzomg@users.noreply.github.com>
2020-09-25 18:01:13 +0700

    Update Main.java

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -3,8 +3,11 @@ import App.App;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        App app = new App("224.0.0.251", 6666);
-        app.run();
+    public static void main(String[] args) {
+        try (App app = new App("224.0.0.251", 6666)) {
+            app.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
